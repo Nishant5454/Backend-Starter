63d83377a550a0667f28e62c4cecc2fb4f84b107
Nishant5454 <bhagatnishant5454@gmail.com>
2024-01-03 09:08:20 +0530

    server started

---
+++ server.js
@@ -0,0 +1,30 @@
+const http=require('http');
+const fs=require('fs');
+const { error } = require('console');
+const server=http.createServer((req,res)=>{
+    console.log('server created');
+    //console.log(req.method); 
+    //console.log(req.url); 
+    //res.setHeader('Content-type','Text/Html'); 
+    //res.write('<h1>Hi Nishant </h1>');
+    //res.write('<h2>Learning  Engg</h2>') 
+   //res.end();
+   fs.readFile('./render/babar2.html',(error,datafile)=>{
+    if(error){
+        console.log("File Error Found");
+    }
+    else{
+        res.end(datafile);
+    }
+
+   })
+    
+
+});
+server.listen(3000,'localhost',()=>{  
+    console.log('server is listning');
+
+
+
+});
+
